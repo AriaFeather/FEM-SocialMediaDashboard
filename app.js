a3c990b0abf25ac3f4b6a871f333bcd76b2f063a
Leon Smith Jr <creativeinspiration101@gmail.com>
2020-11-12 14:23:33 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(express.static("public"));
+app.set(bodyParser.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+let port = process.env.PORT;
+if (port == null || port == ""){
+    port = 3000;
+}
+
+app.listen(port, () => {
+        console.log("Server Started!");
+    });
